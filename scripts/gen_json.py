5e6990b2e51e6a2f95eb51d54eec1173bf064d9e
Samuel Albrecht <elsamuko@gmail.com>
2020-12-31 20:07:51 +0100

    chg: updated gen_json.py

--- scripts/gen_json.py
+++ scripts/gen_json.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 # python3 -m pip install pandas odfpy
+# ./scripts/gen_json.py res/exercises.ods > res/exercises.json
 
 import pandas as pd
 import sys
 import json
 
-# filename = sys.argv[1]
-filename = "../res/exercises.ods"
-
+# open exercises.odf
+filename = sys.argv[1]
 df = pd.read_excel(filename, engine="odf")
+df = df.fillna(0)
 # print(df)
 
+# parse spreadsheet
 exercises = []
-df.apply(lambda row: exercises.append({"name":row["Exercise"]}), axis=1)
+df.apply(lambda row: exercises.append(row.to_dict()), axis=1)
 
+# dump as json
 print(json.dumps(exercises))
