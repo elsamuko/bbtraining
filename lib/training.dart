f450e9ecb1d7f95764e712c086398ae7b36c3cf3
Samuel Albrecht <elsamuko@gmail.com>
2021-01-02 00:54:38 +0100

    chg: use filter to generate well-balanced trainings

--- lib/training.dart
+++ lib/training.dart
@@ -1,5 +1,4 @@
 import 'exercise.dart';
-import 'dart:math';
 
 extension RangeExtension on int {
   List<int> to(int to) => [for (int i = this; i <= to; i++) i];
@@ -10,26 +9,30 @@ class Training {
   List<Exercise> second = List(3);
   List<Exercise> third = List(3);
 
-  static Training gen_training(List<Exercise> exercises) {
+  static Training genTraining(List<Exercise> exercises) {
     Training training = Training();
 
+    Requirement lower = Requirement("lower", (Exercise exercise) => exercise.isLower());
+    Requirement upper = Requirement("upper", (Exercise exercise) => exercise.isUpper());
+    Requirement core = Requirement("core", (Exercise exercise) => exercise.isCore());
+    Requirement cardio = Requirement("cardio", (Exercise exercise) => exercise.isCardio());
+    Requirement strength = Requirement("strength", (Exercise exercise) => exercise.isStrength());
+    Requirement mobility = Requirement("mobility", (Exercise exercise) => exercise.isMobility());
+
     // warm up/cardio
-    0.to(2).forEach((pos) {
-      Exercise exercise = exercises[Random().nextInt(exercises.length)];
-      training.first[pos] = exercise;
-    });
+    training.first[0] = Exercise.randomWithRequirements(exercises, [cardio, lower]);
+    training.first[1] = Exercise.randomWithRequirements(exercises, [cardio, core]);
+    training.first[2] = Exercise.randomWithRequirements(exercises, [cardio, lower]);
 
     // strength
-    0.to(2).forEach((pos) {
-      Exercise exercise = exercises[Random().nextInt(exercises.length)];
-      training.second[pos] = exercise;
-    });
+    training.second[0] = Exercise.randomWithRequirements(exercises, [strength, lower]);
+    training.second[1] = Exercise.randomWithRequirements(exercises, [strength, upper]);
+    training.second[2] = Exercise.randomWithRequirements(exercises, [strength, lower]);
 
     // mobility
-    0.to(2).forEach((pos) {
-      Exercise exercise = exercises[Random().nextInt(exercises.length)];
-      training.third[pos] = exercise;
-    });
+    training.third[0] = Exercise.randomWithRequirements(exercises, [mobility, lower]);
+    training.third[1] = Exercise.randomWithRequirements(exercises, [mobility, core]);
+    training.third[2] = Exercise.randomWithRequirements(exercises, [mobility, lower]);
 
     return training;
   }
