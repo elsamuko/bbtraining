a7f4b6cc0956202b75525a7668583bffbbb89bdd
Samuel Albrecht <elsamuko@gmail.com>
2021-01-02 23:17:30 +0100

    chg: avoid dups when creating a training

--- lib/training.dart
+++ lib/training.dart
@@ -9,6 +9,10 @@ class Training {
   List<Exercise> second = List(3);
   List<Exercise> third = List(3);
 
+  bool contains(Exercise a) {
+    return first.any((b) => a == b) || second.any((b) => a == b) || third.any((b) => a == b);
+  }
+
   static Training genTraining(List<Exercise> exercises) {
     Training training = Training();
 
@@ -19,20 +23,26 @@ class Training {
     Requirement strength = Requirement("strength", (Exercise exercise) => exercise.isStrength());
     Requirement mobility = Requirement("mobility", (Exercise exercise) => exercise.isMobility());
 
+    Requirement indoor = Requirement("indoor", (Exercise exercise) => exercise.isIndoor());
+    Requirement toolless = Requirement("toolless", (Exercise exercise) => exercise.isToolless());
+    Requirement noDuplicates = Requirement("noDuplicates", (Exercise a) => !training.contains(a));
+
+    List<Requirement> all = [indoor, toolless, noDuplicates];
+
     // warm up/cardio
-    training.first[0] = Exercise.randomWithRequirements(exercises, [cardio, lower]);
-    training.first[1] = Exercise.randomWithRequirements(exercises, [cardio, core]);
-    training.first[2] = Exercise.randomWithRequirements(exercises, [cardio, lower]);
+    training.first[0] = Exercise.randomWithRequirements(exercises, all + [cardio, lower]);
+    training.first[1] = Exercise.randomWithRequirements(exercises, all + [cardio, core]);
+    training.first[2] = Exercise.randomWithRequirements(exercises, all + [cardio, lower]);
 
     // strength
-    training.second[0] = Exercise.randomWithRequirements(exercises, [strength, lower]);
-    training.second[1] = Exercise.randomWithRequirements(exercises, [strength, upper]);
-    training.second[2] = Exercise.randomWithRequirements(exercises, [strength, lower]);
+    training.second[0] = Exercise.randomWithRequirements(exercises, all + [strength, lower]);
+    training.second[1] = Exercise.randomWithRequirements(exercises, all + [strength, upper]);
+    training.second[2] = Exercise.randomWithRequirements(exercises, all + [strength, lower]);
 
     // mobility
-    training.third[0] = Exercise.randomWithRequirements(exercises, [mobility, lower]);
-    training.third[1] = Exercise.randomWithRequirements(exercises, [mobility, core]);
-    training.third[2] = Exercise.randomWithRequirements(exercises, [mobility, lower]);
+    training.third[0] = Exercise.randomWithRequirements(exercises, all + [mobility, lower]);
+    training.third[1] = Exercise.randomWithRequirements(exercises, all + [mobility, core]);
+    training.third[2] = Exercise.randomWithRequirements(exercises, all + [mobility, lower]);
 
     return training;
   }
