6067af5ef090a0e8123918bc7a44fa5b03805dfc
Samuel Albrecht <elsamuko@gmail.com>
2021-01-11 20:08:18 +0100

    chg: separated filtering and selection

--- lib/exercise.dart
+++ lib/exercise.dart
@@ -64,8 +64,13 @@ class Exercise {
 
   String toString() => name;
 
-  static Exercise randomWithRequirements(List<Exercise> exercises, List<Requirement> requirements) {
+  static List<Exercise> allWithRequirements(List<Exercise> exercises, List<Requirement> requirements) {
     List<Exercise> filtered = exercises.where((exercise) => requirements.every((filter) => filter(exercise))).toList();
+    return filtered;
+  }
+
+  static Exercise randomWithRequirements(List<Exercise> exercises, List<Requirement> requirements) {
+    List<Exercise> filtered = allWithRequirements(exercises, requirements);
     return filtered[Random().nextInt(filtered.length)];
   }
 
