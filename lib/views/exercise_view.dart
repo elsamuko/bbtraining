7850dfe2839f4dc430fbdf465b0a89426340416f
Samuel Albrecht <elsamuko@gmail.com>
2021-01-24 21:59:44 +0100

    add: exercise_view

---
+++ lib/views/exercise_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../exercise.dart';
+
+extension RangeExtension on int {
+  List<Widget> each(Widget w) => [for (int i = 0; i < this; i++) w];
+}
+
+class ExerciseView extends StatefulWidget {
+  Exercise exercise;
+
+  ExerciseView(this.exercise, {Key key}) : super(key: key);
+
+  @override
+  ExerciseViewState createState() => ExerciseViewState();
+}
+
+class ExerciseViewState extends State<ExerciseView> {
+  Row stars(int count, IconData icon) {
+    return Row(
+      children: count.each(Row(children: [SizedBox(width: 3), FaIcon(icon, size: 20, color: Colors.red)])),
+    );
+  }
+
+  Widget line(String label, int count, IconData icon) {
+    return Row(
+      children: [
+        Expanded(child: Text(label)),
+        stars(count, icon),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.exercise.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  children: [
+                    line("Upper", widget.exercise.upper, FontAwesomeIcons.arrowAltCircleUp),
+                    line("Lower", widget.exercise.lower, FontAwesomeIcons.arrowAltCircleDown),
+                    line("Core", widget.exercise.core, FontAwesomeIcons.dotCircle),
+                    line("Strength", widget.exercise.strength, FontAwesomeIcons.fistRaised),
+                    line("Cardio", widget.exercise.cardio, FontAwesomeIcons.heartbeat),
+                    line("Mobility", widget.exercise.mobility, FontAwesomeIcons.expandArrowsAlt),
+                    line("Difficulty", widget.exercise.difficulty, FontAwesomeIcons.star),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
