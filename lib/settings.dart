aa2e62c8332df3b420e7414df32abf87e6daf939
Samuel Albrecht <elsamuko@gmail.com>
2021-02-03 21:51:24 +0100

    add: Settings class

---
+++ lib/settings.dart
@@ -0,0 +1,32 @@
+enum Repetitions { Easy, Normal, Hard }
+
+extension Name on Repetitions {
+  String get name {
+    switch (this) {
+      case Repetitions.Easy:
+        return "Easy";
+      case Repetitions.Hard:
+        return "Hard";
+      default:
+        return "Normal";
+    }
+  }
+}
+
+class Settings {
+  bool useWeights = false;
+  bool useBank = true;
+  bool useBar = false;
+  bool withOutdoor = false;
+  Repetitions repetitions = Repetitions.Normal;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'useWeights': useWeights,
+      'useBank': useBank,
+      'useBar': useBar,
+      'withOutdoor': withOutdoor,
+      'Repetitions': repetitions,
+    };
+  }
+}
