5c2b5e0049d9bfba1ae6bf4b73045a90de55ac6d
Samuel Albrecht <elsamuko@gmail.com>
2021-02-08 21:15:16 +0100

    add: unit test for Settings class

---
+++ test/settings_test.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:bbtraining/level.dart';
+import 'package:bbtraining/settings.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('IO', () {
+    Settings settings = Settings();
+
+    // parse toMap back to cluster
+    Map<String, dynamic> m = settings.toMap();
+    Settings settings2 = Settings.fromMap(m);
+    expect(settings, settings2);
+
+    // parse toJSON back to cluster
+    String json = jsonEncode(settings);
+    Map<String, dynamic> m2 = jsonDecode(json);
+    Settings settings3 = Settings.fromMap(m2);
+    expect(settings, settings3);
+  });
+}
