0c4c1f6d8b755d603b81c51185ff9c98eaf61da1
Samuel Albrecht <elsamuko@gmail.com>
2021-02-08 21:15:05 +0100

    add: operator==, from/toMap, toJson, hashCode to Settings class

--- lib/settings.dart
+++ lib/settings.dart
@@ -7,8 +7,18 @@ class Settings {
   bool withOutdoor = false;
   Level level = Level.Normal;
 
-  bool hard() => level == Level.Hard;
-  bool easy() => level == Level.Easy;
+  @override
+  bool operator ==(dynamic other) {
+    if (this.useWeights != other.useWeights) return false;
+    if (this.useBank != other.useBank) return false;
+    if (this.useBar != other.useBar) return false;
+    if (this.withOutdoor != other.withOutdoor) return false;
+    if (this.level != other.level) return false;
+    return true;
+  }
+
+  @override
+  int get hashCode => useWeights.hashCode ^ useBank.hashCode ^ useBar.hashCode ^ withOutdoor.hashCode ^ level.hashCode;
 
   Map<String, dynamic> toMap() {
     return {
@@ -16,7 +26,20 @@ class Settings {
       'useBank': useBank,
       'useBar': useBar,
       'withOutdoor': withOutdoor,
-      'level': level,
+      'level': level.index,
     };
+  }
+
+  Map<String, dynamic> toJson() => toMap();
+
+  static Settings fromMap(Map<String, dynamic> data) {
+    Settings settings = Settings();
+    settings.useWeights = (data['useWeights'] ?? false);
+    settings.useBank = (data['useBank'] ?? true);
+    settings.useBar = (data['useBar'] ?? false);
+    settings.withOutdoor = (data['withOutdoor'] ?? false);
+    settings.level = Level.values[(data['level'] ?? 1)];
+
+    return settings;
   }
 }
