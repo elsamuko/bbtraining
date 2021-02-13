7301a202e4fcc05f2d7595a03febaf2aa68135f2
Samuel Albrecht <elsamuko@gmail.com>
2021-02-13 20:51:46 +0100

    add: bash script to generate screenshots

---
+++ screenshots.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# start and forward emulator
+# flutter emulators --launch Nexus_5_API_24
+adb forward tcp:3001 tcp:3001
+
+# take screenshots
+flutter clean
+flutter drive --target=test_driver/screenshot.dart
+
+cd screenshots || exit
+
+for SHOT in screenshot_*.png; do
+  echo "Adding title to $SHOT"
+  convert "$SHOT" -page +0+0 ../res/title.png -flatten "$SHOT"
+done
+
+echo "Screenshots done :)"
+zenity --info --no-wrap --title "BB Training" --text "Screenshots done :)"
