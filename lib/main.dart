b853b2449744183d587fa1f1cb40be8b8f6ce405
Samuel Albrecht <elsamuko@gmail.com>
2021-02-14 11:09:00 +0100

    chg: darker scaffold background color

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'BB Training',
       theme: ThemeData(
         brightness: Brightness.dark,
+        scaffoldBackgroundColor: Color(0xff222222),
         primaryColor: Color(0xff00835c),
         accentColor: Color(0xff800060),
         buttonColor: Color(0xff547390),
