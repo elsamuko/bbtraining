465bee0c9e4cb47b4d3625333d724b16ed64164a
Samuel Albrecht <elsamuko@gmail.com>
2021-03-06 20:18:48 +0100

    add: count of available images for each exercise into json

--- scripts/gen_json.py
+++ scripts/gen_json.py
@@ -5,6 +5,20 @@
 import pandas as pd
 import sys
 import json
+import glob
+import os
+
+
+def add_image_count(exercise):
+    """
+    images for exercises are in res/images/${exercise}_#.jpg
+    """
+    glob_name = image_path + exercise["exercise"] + "*"
+    exercise['images'] = len(glob.glob(glob_name))
+
+
+# path to ./scripts/../res/images
+image_path = os.path.dirname(os.path.realpath(__file__)) + "/../res/images/"
 
 # open exercises.odf
 filename = sys.argv[1]
@@ -15,6 +29,9 @@ df = df.fillna(0)
 # parse spreadsheet
 exercises = []
 df.apply(lambda row: exercises.append(row.to_dict()), axis=1)
+
+# add number of images for this exercise in res/images
+[add_image_count(exercise) for exercise in exercises]
 
 # dump as json
 print(json.dumps(exercises))
