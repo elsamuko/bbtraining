304a7dc2fb2ca575c8453057fb092e537fded47c
Samuel Albrecht <elsamuko@gmail.com>
2021-06-07 21:26:22 +0200

    chg: print all trainings

--- scripts/gen_training.dart
+++ scripts/gen_training.dart
@@ -2,8 +2,11 @@
 
 import 'dart:io';
 import 'dart:convert';
+
 import '../lib/settings.dart';
 import '../lib/trainings/functional.dart';
+import '../lib/trainings/core.dart';
+import '../lib/trainings/mobility.dart';
 import '../lib/exercise.dart';
 
 int main(List<String> args) {
@@ -20,6 +23,10 @@ int main(List<String> args) {
   Settings settings = Settings();
   FunctionalTraining training = FunctionalTraining.genTraining(exercises, settings);
   print(training);
+  MobilityTraining mobility = MobilityTraining.genTraining(exercises, settings);
+  print(mobility);
+  CoreTraining core = CoreTraining.genTraining(exercises, settings);
+  print(core);
 
   return 0;
 }
