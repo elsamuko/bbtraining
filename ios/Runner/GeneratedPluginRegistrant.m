20a3903d52cbf207930fd30fc05c03c07b55f3ca
Samuel Albrecht <elsamuko@gmail.com>
2021-12-07 21:58:30 +0100

    chg: updated auto-generated files

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -12,10 +12,10 @@
 @import package_info;
 #endif
 
-#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
-#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#if __has_include(<shared_preferences_ios/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences_ios/FLTSharedPreferencesPlugin.h>
 #else
-@import shared_preferences;
+@import shared_preferences_ios;
 #endif
 
 @implementation GeneratedPluginRegistrant
