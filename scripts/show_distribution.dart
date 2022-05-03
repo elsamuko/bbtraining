310fce25cd2733e94df316db2a8d3d0f15cd9550
Samuel Albrecht <elsamuko@gmail.com>
2022-05-03 18:34:10 +0200

    chg: updated show_distribution.dart

--- scripts/show_distribution.dart
+++ scripts/show_distribution.dart
@@ -2,10 +2,12 @@
 
 import 'dart:io';
 import 'dart:convert';
+import 'dart:collection';
 
 import 'package:bbtraining/settings.dart';
 import 'package:bbtraining/trainings/functional.dart';
 import 'package:bbtraining/exercise.dart';
+import 'package:sprintf/sprintf.dart';
 
 int main(List<String> args) {
   if (args.isEmpty) {
@@ -23,7 +25,7 @@ int main(List<String> args) {
     countsRef[e] = 0;
   });
   Settings settings = Settings();
-  settings.useWeights = true;
+  settings.useWeights = false;
   settings.useBar = true;
   settings.withOutdoor = true;
 
@@ -39,8 +41,15 @@ int main(List<String> args) {
     });
   }
 
-  countsRand.forEach((key, value) {
-    print("${countsRef[key]} : $value $key");
+  final sorted = SplayTreeMap<Exercise, int>.from(countsRand, (l, r) => countsRand[l]!.compareTo(countsRand[r]!));
+  sorted.forEach((key, value) {
+    var c = key.isCardio() ? "C" : "_";
+    var s = key.isStrength() ? "S" : "_";
+    var m = key.isMobility() ? "M" : "_";
+    var u = key.isUpper() ? "U" : "_";
+    var l = key.isLower() ? "L" : "_";
+
+    print(sprintf("%1i : %4i [$c$s$m$u$l] %.2f $key", [countsRef[key], value, key.weight]));
   });
 
   return 0;
