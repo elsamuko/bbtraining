33fc04839fb2acb5a1f5c83c3737b0c23e65e49e
Samuel Albrecht <elsamuko@gmail.com>
2023-02-05 11:21:13 +0100

    chg: updated main colors

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
       title: 'BB Training',
       theme: ThemeData(
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xff222222),
+        scaffoldBackgroundColor: const Color(0xff111111),
         visualDensity: VisualDensity.adaptivePlatformDensity,
         colorScheme: const ColorScheme.dark(
-          surface: const Color(0xff00835c),
+          surface: const Color(0xff007680),
           primary: Colors.white,
           primaryContainer: const Color(0xff547390),
-          secondary: const Color(0xff800060),
+          secondary: const Color(0xff9a007d),
         ),
       ),
       home: TrainingView(),
